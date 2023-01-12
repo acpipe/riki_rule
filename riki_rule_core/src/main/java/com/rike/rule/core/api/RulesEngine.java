61503bbb5b754d41260dcc87ff7b06ca0bd90720
huming <1023598954@qq.com>
2023-01-12 19:54:40 +0800

    check and executor

--- riki_rule_core/src/main/java/com/rike/rule/core/api/RulesEngine.java
+++ riki_rule_core/src/main/java/com/rike/rule/core/api/RulesEngine.java
@@ -1,9 +1,12 @@
 package com.rike.rule.core.api;
 
+import java.util.Map;
+
 /**
  * 执行引擎接口, 定义成接口方便后面扩展.
  * @author huming on 2023/1/12.
  */
 public interface RulesEngine {
-    Map<Rule, Boolean> check(Rule)
+    Map<Rule, Boolean> check(Rules rules, Facts facts);
+
 }
