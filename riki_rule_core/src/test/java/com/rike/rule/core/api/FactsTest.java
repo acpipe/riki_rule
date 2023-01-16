6adf21c78efc073f137dd5cc6891d6bba6c31693
huming <1023598954@qq.com>
2023-01-16 17:31:39 +0800

    version

---
+++ riki_rule_core/src/test/java/com/rike/rule/core/api/FactsTest.java
@@ -0,0 +1,15 @@
+package com.rike.rule.core.api;
+
+import java.util.Map;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author huming on 2023/1/16.
+ */
+public class FactsTest {
+
+    public void facts() {
+
+    }
+}
