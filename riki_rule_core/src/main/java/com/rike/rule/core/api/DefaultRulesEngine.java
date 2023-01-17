00703928ce28abf932cd0957bfdfa51d4802af45
huming <1023598954@qq.com>
2023-01-17 14:42:34 +0800

    log error

--- riki_rule_core/src/main/java/com/rike/rule/core/api/DefaultRulesEngine.java
+++ riki_rule_core/src/main/java/com/rike/rule/core/api/DefaultRulesEngine.java
@@ -3,10 +3,12 @@ package com.rike.rule.core.api;
 import java.util.HashMap;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author huming on 2023/1/13.
  */
+@Slf4j
 public class DefaultRulesEngine implements RulesEngine {
     @Override
     public Map<Rule, Boolean> check(Rules rules, Facts facts) {
@@ -19,6 +21,7 @@ public class DefaultRulesEngine implements RulesEngine {
                     rule.execute(facts);
                 } catch (Exception e) {
                     // log
+                    log.error("执行规则错误", e);
                 }
             }
         });
